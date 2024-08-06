0e9c9412ce14b74ca49aea57c16f1db11452bc95
sahilbadgujar <113436650+sahilbadgujar@users.noreply.github.com>
2024-08-06 23:54:43 +0530

    text_to_speech.py

--- text_to_speech.py
+++ text_to_speech.py
@@ -1,6 +1,12 @@
 from gtts import gTTS
-language = "en"
-text = "open google"
-speech = gTTS(text = text)
-speech.save("open_google.mp3")
-#0728,2228
+import os
+
+def text_to_speech(text, language='en', output_file='output.mp3'):
+    tts = gTTS(text=text, lang=language, slow=False)
+    tts.save(output_file)
+    print(f"Saved to {output_file}")
+
+if __name__ == "__main__":
+    text = "Hello, this is a text to speech conversion example."
+    text_to_speech(text)
+    os.system("start output.mp3")  
